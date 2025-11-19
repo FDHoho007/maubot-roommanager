21e2eef409b20cc4b0a5ef48c2caa7ba22038530
FDHoho007 <mail@fdhoho007.de>
2025-11-19 15:16:54 +0100

    Made room ids optional

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+# v1.1.0
+
+Made room ids in commands optional. If no room id is supplied the current room is used.
+
 # v1.0.0
 
 Initial version
--- roommanager.py
+++ roommanager.py
@@ -4,7 +4,7 @@ from maubot import Plugin, MessageEvent
 from maubot.handlers import command
 from mautrix.api import Method, Path
 from mautrix.util.config import BaseProxyConfig, ConfigUpdateHelper
-from mautrix.types import RoomDirectoryVisibility, Membership, EventType, TextMessageEventContent, PowerLevelStateEventContent, RoomType
+from mautrix.types import RoomDirectoryVisibility, Membership, EventType, TextMessageEventContent, PowerLevelStateEventContent, RoomType, RoomID
 
 ROOM_VERSION = "12"
 
@@ -34,7 +34,7 @@ class RoomManager(Plugin):
         if len(rooms) == 0:
             await evt.reply("No rooms created by this Room Manager instance were found.", allow_html=True)
         else:
-            room_mentions = [f"{self.mention_room(r)} ({'Space' if rooms[r] == RoomType.SPACE else 'Room'})" for r in rooms.keys()]
+            room_mentions = [f"{self.mention_mxid(r)} ({'Space' if rooms[r] == RoomType.SPACE else 'Room'})" for r in rooms.keys()]
             await evt.reply(f"Rooms created by this Room Manager instance:\n" + "\n".join(room_mentions), allow_html=True)
 
     @command.new(help="Creates a new room and adds you as an administrator.")
@@ -78,34 +78,30 @@ class RoomManager(Plugin):
             room_version=ROOM_VERSION, 
             power_level_override={"users": {evt.sender: 100}}
         ))
-        await evt.reply(f"Created {room_type} {self.mention_room(room_id)} with visibility {visibility[0]}.", allow_html=True)
+        await evt.reply(f"Created {room_type} {self.mention_mxid(room_id)} with visibility {visibility[0]}.", allow_html=True)
 
     @command.new(help=f"Upgrade a room to version {ROOM_VERSION} (only for room admins).")
-    @command.argument("room_id", label="Room ID", required=True)
+    @command.argument("room_id", label="Room ID", required=False)
     async def upgraderoom(self, evt: MessageEvent, room_id: str) -> None:
-        room_id = room_id.rstrip("<br/>").strip()
-        if room_id == "":
-            await evt.reply("Please provide a valid room ID.")
-            return
-        room_id = self.parse_args(evt.content, extract_user_id=False)
+        room_id = self.parse_args(evt.content, evt.room_id, extract_user_id=False)
 
         # Check if the room exists and can be upgraded
         try:
             create_event = await self.client.get_state_event(room_id, EventType.ROOM_CREATE, "")
             if create_event.type != None:
-                await evt.reply(f"The room {self.mention_room(room_id)} is a space. I can only upgrade rooms, not spaces.", allow_html=True)
+                await evt.reply(f"The room {self.mention_mxid(room_id)} is a space. I can only upgrade rooms, not spaces.", allow_html=True)
                 return
             if create_event.room_version >= ROOM_VERSION:
-                await evt.reply(f"The room {self.mention_room(room_id)} is already on version {create_event.room_version}. I currently uses room version {ROOM_VERSION}.", allow_html=True)
+                await evt.reply(f"The room {self.mention_mxid(room_id)} is already on version {create_event.room_version}. I currently uses room version {ROOM_VERSION}.", allow_html=True)
                 return
         except Exception:
-            await evt.reply(f"The room {self.mention_room(room_id)} does not exist or I am not a member of it.", allow_html=True)
+            await evt.reply(f"The room {self.mention_mxid(room_id)} does not exist or I am not a member of it.", allow_html=True)
             return
         
         # Check if the room has not yet been upgraded
         try:
             await self.client.get_state_event(room_id, EventType.ROOM_TOMBSTONE, "")
-            await evt.reply(f"The room {self.mention_room(room_id)} has already been upgraded once and cannot be upgraded again.", allow_html=True)
+            await evt.reply(f"The room {self.mention_mxid(room_id)} has already been upgraded once and cannot be upgraded again.", allow_html=True)
             return
         except Exception:
             pass
@@ -122,15 +118,15 @@ class RoomManager(Plugin):
             new_room_id = (await self.client.api.request(Method.POST, Path.v3.rooms[room_id].upgrade, {"new_version": ROOM_VERSION}))["replacement_room"]
             for member in [m for m in room_members if not m == self.client.mxid]:
                 await self.client.invite_user(new_room_id, member)
-            await evt.reply(f"Room {self.mention_room(room_id)} has been upgraded to v{ROOM_VERSION}.", allow_html=True)
+            await evt.reply(f"Room {self.mention_mxid(room_id)} has been upgraded to v{ROOM_VERSION}.", allow_html=True)
         except Exception:
-            await evt.reply(f"Could not upgrade the room {self.mention_room(room_id)}. Make sure I have sufficient permissions.", allow_html=True)
+            await evt.reply(f"Could not upgrade the room {self.mention_mxid(room_id)}. Make sure I have sufficient permissions.", allow_html=True)
 
     @command.new(help="Add another administrator to a given room (only for existing room admins).")
-    @command.argument("room_id", label="Room ID", required=True)
     @command.argument("user_id", label="User ID", required=True)
-    async def addadmin(self, evt: MessageEvent, room_id: str, user_id: str) -> None:
-        room_id, user_id = self.parse_args(evt.content)
+    @command.argument("room_id", label="Room ID", required=False)
+    async def addadmin(self, evt: MessageEvent, user_id: str, room_id: str) -> None:
+        room_id, user_id = self.parse_args(evt.content, evt.room_id)
 
         try:
             room_members, power_levels = await self.get_room_members(room_id)
@@ -146,23 +142,24 @@ class RoomManager(Plugin):
                 if not user_id in room_members:
                     await self.client.invite_user(room_id, user_id)
             except Exception:
-                raise Exception(f"Could not find user with ID {user_id} or invite failed.")
+                raise Exception(f"Could not find user with ID {self.mention_mxid(user_id)} or invite failed.")
 
             # If the user is not yet an admin, promote them
             if power_levels.users.get(user_id, 0) < 100:
                 power_levels.users[user_id] = 100
                 await self.client.send_state_event(room_id, EventType.ROOM_POWER_LEVELS, power_levels)
 
-            await evt.reply(f"User {user_id} has been promoted to administrator in room {self.mention_room(room_id)}.", allow_html=True)
+            await evt.reply(f"User {self.mention_mxid(user_id)} has been promoted to administrator in room {self.mention_mxid(room_id)}.", allow_html=True)
         except Exception as e:
             await evt.reply(e.args[0], allow_html=True)
             return
 
     @command.new(help="Demotes a room administrator to normal user (only for room admins).")
-    @command.argument("room_id", label="Room ID", required=True)
     @command.argument("user_id", label="User ID", required=True)
-    async def removeadmin(self, evt: MessageEvent, room_id: str, user_id: str) -> None:
-        room_id, user_id = self.parse_args(evt.content)
+    @command.argument("room_id", label="Room ID", required=False)
+    async def removeadmin(self, evt: MessageEvent, user_id: str, room_id: str) -> None:
+        room_id, user_id = self.parse_args(evt.content, evt.room_id)
+        self.log.info(f"Demoting user {user_id} in room {room_id}")
 
         try:
             room_members, power_levels = await self.get_room_members(room_id)
@@ -175,25 +172,22 @@ class RoomManager(Plugin):
 
             # Check if the user is even a member
             if not user_id in room_members:
-                raise Exception(f"The user {user_id} is not a member of the room.")
+                raise Exception(f"The user {self.mention_mxid(user_id)} is not a member of the room.")
 
             # If the user is an admin, demote them to normal user
             if power_levels.users.get(user_id, 0) == 100:
                 power_levels.users[user_id] = 0
                 await self.client.send_state_event(room_id, EventType.ROOM_POWER_LEVELS, power_levels)
 
-            await evt.reply(f"User {user_id} has been demoted from administrator in room {self.mention_room(room_id)}.", allow_html=True)
+            await evt.reply(f"User {self.mention_mxid(user_id)} has been demoted from administrator in room {self.mention_mxid(room_id)}.", allow_html=True)
         except Exception as e:
             await evt.reply(e.args[0], allow_html=True)
             return
 
     @command.new(help="Promote yourself to a room administrator (only for instance admins).")
-    @command.argument("room_id", label="Room ID", required=True)
+    @command.argument("room_id", label="Room ID", required=False)
     async def becomeadmin(self, evt: MessageEvent, room_id: str) -> None:
-        if room_id.strip() == "":
-            await evt.reply("Please provide a valid room ID.")
-            return
-        room_id = self.parse_args(evt.content, extract_user_id=False)
+        room_id = self.parse_args(evt.content, evt.room_id, extract_user_id=False)
 
         try:
             room_members, power_levels = await self.get_room_members(room_id)
@@ -212,25 +206,31 @@ class RoomManager(Plugin):
                 power_levels.users[evt.sender] = 100
                 await self.client.send_state_event(room_id, EventType.ROOM_POWER_LEVELS, power_levels)
 
-            await evt.reply(f"You have been promoted to administrator in room {self.mention_room(room_id)}.", allow_html=True)
+            await evt.reply(f"You have been promoted to administrator in room {self.mention_mxid(room_id)}.", allow_html=True)
         except Exception as e:
             await evt.reply(e.args[0], allow_html=True)
             return
 
-    def parse_args(self, content: TextMessageEventContent, extract_user_id: bool = True) -> str | tuple[str, str]:
+    def parse_args(self, content: TextMessageEventContent, room_id: RoomID, extract_user_id: bool = True) -> str | tuple[str, str]:
         """Parses either room_id or room_id and user_id as arguments.
         If a mention is used and represented as an HTML link, the full Matrix ID is extracted.
         """
         command_raw = content.formatted_body if content.formatted_body else content.body
+        command_raw = command_raw.strip()
+        while command_raw.endswith("<br/>"):
+            command_raw = command_raw[:-5].strip()
+        # Mentioning a user can look like this: <a href="https://matrix.to/#/@user:server">@user</a>
+        # Only the html variant contains the full Matrix ID, since the plain text variant removes the a tag and only keeps @user
+        command_raw = re.sub(r'<a\s+href=[\'"]https?://matrix\.to/#/([^\'"]+)[\'"]>[^<]*</a>', r'\1', command_raw, flags=re.IGNORECASE)
         if extract_user_id:
-            _, room_id, user_id = command_raw.split(" ", 2)
-            room_id = self.unpack_html_link("#|!", room_id)
-            user_id = self.unpack_html_link("@", user_id)
-            return room_id, user_id
+            args = command_raw.split()
+            #user_id = self.unpack_html_link("@", args[1])
+            #room_id = self.unpack_html_link("#|!", args[2]) if len(args) > 2 else room_id
+            return args[2] if len(args) > 2 else room_id, args[1]
         else:
-            _, room_id = command_raw.split(" ", 1)
-            room_id = self.unpack_html_link("#|!", room_id)
-            return room_id
+            args = command_raw.split()
+            #room_id = self.unpack_html_link("#|!", args[1]) if len(args) > 1 else room_id
+            return args[1] if len(args) > 1 else room_id
 
     def unpack_html_link(self, prefix_token: str, text: str) -> str:
         # Mentioning a user can look like this: <a href="https://matrix.to/#/@user:server">@user</a>
@@ -240,16 +240,16 @@ class RoomManager(Plugin):
             return m.group(2)
         return text.rstrip("<br/>").strip()
     
-    def mention_room(self, room_id: str) -> str:
-        return f'<a href="https://matrix.to/#/{room_id}">{room_id}</a>'
+    def mention_mxid(self, mxid: str) -> str:
+        return f'<a href="https://matrix.to/#/{mxid}">{mxid}</a>'
 
     async def assert_room_version(self, room_id: str) -> None:
         """Asserts that the room is of the supported ROOM_VERSION and was created by the bot itself."""
         room_creation_event = [e for e in (await self.client.get_state(room_id)) if e.type == EventType.ROOM_CREATE][0]
         if room_creation_event.content.room_version != ROOM_VERSION:
-            raise Exception(f"I only support rooms with version {ROOM_VERSION}. The room {self.mention_room(room_id)} has version {room_creation_event.content.room_version}.")
+            raise Exception(f"I only support rooms with version {ROOM_VERSION}. The room {self.mention_mxid(room_id)} has version {room_creation_event.content.room_version}.")
         if room_creation_event.sender != self.client.mxid:
-            raise Exception(f"I only manage rooms created by myself. The room {self.mention_room(room_id)} was created by {room_creation_event.sender}.")
+            raise Exception(f"I only manage rooms created by myself. The room {self.mention_mxid(room_id)} was created by {self.mention_mxid(room_creation_event.sender)}.")
 
     async def assert_room_admin(self, room_members: list[str], power_levels: PowerLevelStateEventContent, user_id: str) -> None:
         """Asserts that the user is an admin in the room."""
@@ -265,5 +265,5 @@ class RoomManager(Plugin):
             power_levels = await self.client.get_state_event(room_id, EventType.ROOM_POWER_LEVELS, "")
             return room_members, power_levels
         except Exception:
-            raise Exception(f"The room {self.mention_room(room_id)} does not exist or I am not a member of it.")
+            raise Exception(f"The room {self.mention_mxid(room_id)} does not exist or I am not a member of it.")
         
